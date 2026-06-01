059711ae6000788068b270e24aff3ca9286e0153
deepikae-cd <deepika.eswar1995@gmail.com>
2026-06-01 19:48:11 +0000

    silver layer creation

--- models/silver/participant/participant.sql
+++ models/silver/participant/participant.sql
@@ -1,9 +1,43 @@
-select
-    participant_id,
-    first_name,
-    last_name,
-    dob,
-    gender,
-    status,
-    created_ts
-from {{ source('bronze','participant_raw') }}
+with source_data as (
+
+    select *
+    from {{ source('bronze','participant_raw') }}
+
+),
+
+cleaned as (
+
+    select
+
+        participant_id,
+
+        upper(trim(first_name)) as first_name,
+
+        upper(trim(last_name)) as last_name,
+
+        lower(trim(email)) as email,
+
+        regexp_replace(phone,'[^0-9]','') as phone,
+
+        gender,
+
+        case
+            when status is null then 'ACTIVE'
+            else upper(status)
+        end as status,
+
+        cast(created_ts as timestamp) as created_ts,
+
+        current_timestamp() as load_ts
+
+    from source_data
+
+)
+
+select *
+from cleaned
+qualify row_number()
+over (
+partition by participant_id
+order by created_ts desc
+)=1
