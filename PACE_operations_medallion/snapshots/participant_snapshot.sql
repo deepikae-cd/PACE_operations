19fc68dcc2258b1fc69ae8194622aa31a2862e6a
deepikae-cd <deepika.eswar1995@gmail.com>
2026-06-03 20:38:19 +0000

    fixed

--- PACE_operations_medallion/snapshots/participant_snapshot.sql
+++ PACE_operations_medallion/snapshots/participant_snapshot.sql
@@ -1,11 +1,19 @@
 {% snapshot participant_snapshot %}
 
+{{
+    config(
+        schema='SNAPSHOTS',
+        strategy='timestamp',
+        unique_key='participant_id',
+        updated_at='source_created_ts'
+    )
+}}
+
 select
     participant_id,
     first_name,
     last_name,
     gender,
-    date_of_birth,
     participant_status,
     source_created_ts
 from {{ ref('participant') }}
