1c9355f273c109ef8afceab97d291c83851593d4
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-04 21:32:11 +0000

    test

--- PACE_operations_medallion/macros/generate_schema_name.sql
+++ PACE_operations_medallion/macros/generate_schema_name.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
     {% if custom_schema_name is not none %}
-        {{ custom_schema_name | upper }}
+        {{ custom_schema_name }}
     {% else %}
         {{ target.schema }}
     {% endif %}
