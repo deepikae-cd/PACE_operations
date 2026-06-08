ea50d948cd8157ba5703c952e39c9803201e6699
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-08 23:37:27 +0000

    test

--- PACE_operations_medallion/models/pace_gold_dbt_models/gold_marts_facts/fct_transportation/fct_transportation.sql
+++ PACE_operations_medallion/models/pace_gold_dbt_models/gold_marts_facts/fct_transportation/fct_transportation.sql
@@ -3,12 +3,9 @@ select
     transportation_id,
     participant_id,
     ride_date,
-
     pickup_location,
     dropoff_location,
-
     trip_status,
-
     case when trip_status = 'completed' then 1 else 0 end as completed_flag
 
 from {{ ref('staging_transportation') }}
