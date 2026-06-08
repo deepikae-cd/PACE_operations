6bf92af30a9bf134e16e0807decfe742f38dbf3f
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-08 22:58:24 +0000

    test

--- PACE_operations_medallion/models/pace_gold_dbt_models/facts/fct_appointment/fct_appointment.sql
+++ PACE_operations_medallion/models/pace_gold_dbt_models/facts/fct_appointment/fct_appointment.sql
@@ -8,8 +8,8 @@ select
     center_id,
 
     -- Metrics
-    appointment_date,
-    appointment_status,
+   scheduled_date as appointment_date,
+   appointment_status,
 
     case when appointment_status = 'completed' then 1 else 0 end as completed_flag,
     case when appointment_status = 'no_show' then 1 else 0 end as no_show_flag,
