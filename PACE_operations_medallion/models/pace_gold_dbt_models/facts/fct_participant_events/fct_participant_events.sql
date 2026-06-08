a1040125399ed7bdf9f4362db3abe3701e978184
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-08 23:00:02 +0000

    test

--- PACE_operations_medallion/models/pace_gold_dbt_models/facts/fct_participant_events/fct_participant_events.sql
+++ PACE_operations_medallion/models/pace_gold_dbt_models/facts/fct_participant_events/fct_participant_events.sql
@@ -1,10 +1,8 @@
 select
 
     participant_id,
-
     enrollment_date,
     disenrollment_date,
-
     case when enrollment_date is not null then 1 else 0 end as enrolled_flag,
     case when disenrollment_date is not null then 1 else 0 end as disenrolled_flag
 
