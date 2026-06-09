00ad144ee333d6a17ce968e72357a914aa0352d5
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-09 00:31:05 +0000

    test

--- PACE_operations_medallion/models/pace_gold_dbt_models/gold_marts_facts/fct_meal_delivery/fct_meal_delivery.sql
+++ PACE_operations_medallion/models/pace_gold_dbt_models/gold_marts_facts/fct_meal_delivery/fct_meal_delivery.sql
@@ -1,6 +1,12 @@
 with base as (
 
-    select *
+    select
+        meal_delivery_id as delivery_id,   
+        participant_id,
+        delivery_date,
+        delivery_status,
+        _loaded_at
+
     from {{ ref('enterprise_silver_meal_delivery') }}
 
 ),
@@ -10,12 +16,21 @@ deduplicated as (
     select *,
            row_number() over (
                partition by delivery_id
-               order by loaded_timestamp desc
+               order by _loaded_at desc   
            ) as _rn
     from base
 
 )
 
-select *
+select
+    delivery_id,
+    participant_id,
+    delivery_date,
+    delivery_status,
+    case 
+        when delivery_status = 'DELIVERED' then 1
+        else 0
+    end as delivered_flag
+
 from deduplicated
-where _rn = 1
+where _rn = 1     
