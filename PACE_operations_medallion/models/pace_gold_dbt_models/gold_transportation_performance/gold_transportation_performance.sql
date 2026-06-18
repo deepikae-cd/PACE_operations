e592c7e6efcae4c645c6d8f9e387a779d97c11a1
deepika eswar <deepika.eswar1995@gmail.com>
2026-06-18 00:17:39 +0000

    test

---
+++ PACE_operations_medallion/models/pace_gold_dbt_models/gold_transportation_performance/gold_transportation_performance.sql
@@ -0,0 +1,44 @@
+/*
+===============================================================================
+Model: gold_transportation_performance
+Purpose:
+  Provides transportation performance KPIs at daily level.
+
+Grain:
+  ride_date
+===============================================================================
+*/
+
+with base as (
+
+    select *
+    from {{ ref('enterprise_silver_transportation') }}
+
+),
+
+aggregated as (
+
+    select
+        ride_date,
+
+        count(*) as total_trips,
+        sum(completed_flag) as completed_trips,
+
+        sum(case when trip_status = 'CANCELLED' then 1 else 0 end) as cancelled_trips
+
+    from base
+    group by ride_date
+
+)
+
+select
+    ride_date,
+    total_trips,
+    completed_trips,
+    cancelled_trips,
+
+    completed_trips / nullif(total_trips,0) as completion_rate,
+
+    current_timestamp() as dbt_updated_timestamp
+
+from aggregated
